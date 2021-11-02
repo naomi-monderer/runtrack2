e22cc3c2b100737f711e3b7d01ab1684df1a3380
naomie-monderer <92308722+naomie-monderer@users.noreply.github.com>
2021-11-02 12:04:54 +0100

    exos

---
+++ jour01/job01/index.php
@@ -0,0 +1,16 @@
+<?php
+    $str = "LaPlateforme";
+    $str2 = "Vive";
+    $str3 = "!";
+    $val = 6;
+    $myBool = true;
+    $myBool = false;
+    
+    
+
+
+    echo  $str2 . " " . $str . " " . $str3  . "<br>";
+    echo  $val . "</br>";
+    echo  $val + 4 . "<br>"; 
+    echo  $myBool; 
+?>
